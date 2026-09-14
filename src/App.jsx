9f3ab73053309603498c1c3ec8357ba792c945e3
Robert Hughes <rjh2869@gmail.com>
2026-09-14 15:19:02 -0500

    added episode data and state

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
-export default function App() {
-  // TODO
+import { episodeList } from "./data";
+import { useState } from "react";
+
+function App() {
+  const [episodes] = useState(episodeList);
+  const [selectedEpisode, setSelectedEpisode] = useState();
+
+  return (
+    <>
+      <header>
+        <h1>Dark Echoes</h1>
+      </header>
+
+      <main></main>
+    </>
+  );
 }
+export default App;
